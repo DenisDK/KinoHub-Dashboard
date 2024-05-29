30129d75b20a381cef0439e991ec7ebb335664b3
DenisDK <unworkdk@gmail.com>
2024-05-29 18:41:46 +0300

    Created Data table functionality + update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,11 @@ const nextConfig = {
         hostname: "firebasestorage.googleapis.com",
         pathname: "**",
       },
+      {
+        protocol: "https",
+        hostname: "i.pinimg.com",
+        pathname: "**",
+      },
     ],
   },
 };
--- src/components/DataTable/DataTable.jsx
+++ src/components/DataTable/DataTable.jsx
@@ -1,58 +1,67 @@
-"use client";
-import * as React from "react";
+import { useEffect, useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import { useTheme } from "next-themes";
+import { collection, onSnapshot } from "firebase/firestore";
+import Image from "next/image";
+import { db } from "@/lib/firebase";
 
 const columns = [
   {
-    field: "id",
-    headerName: "ID",
-    width: 70,
-  },
-  {
-    field: "firstName",
-    headerName: "First name",
-    width: 130,
-  },
-  {
-    field: "lastName",
-    headerName: "Last name",
-    width: 130,
-  },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 90,
-  },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params) => {
-      const firstName = params.row.firstName || "";
-      const lastName = params.row.lastName || "";
-      return `${firstName} ${lastName}`;
+    field: "profile_image",
+    headerName: "Avatar",
+    width: 65,
+    renderCell: (params) => {
+      if (!params.value) {
+        return <span>No Avatar</span>;
+      }
+      return (
+        <div style={{ width: 45, height: 45, position: "relative" }}>
+          <Image
+            src={params.value}
+            alt="avatar"
+            fill
+            sizes="50px"
+            style={{ borderRadius: "50%" }}
+          />
+        </div>
+      );
     },
   },
-];
-
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  {
+    field: "nickname",
+    headerName: "Name",
+    width: 130,
+  },
+  {
+    field: "isPremium",
+    headerName: "Premium",
+    width: 130,
+    renderCell: (params) => <span>{params.value ? "Yes" : "No"}</span>,
+  },
+  {
+    field: "isAdmin",
+    headerName: "Admin",
+    width: 130,
+    renderCell: (params) => <span>{params.value ? "Yes" : "No"}</span>,
+  },
 ];
 
 const DataTable = () => {
   const { theme } = useTheme();
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, "Users"), (snapshot) => {
+      const usersList = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setRows(usersList);
+    });
+
+    return () => unsubscribe(); // Cleanup subscription on unmount
+  }, []);
+
   return (
     <div className="w-full h-auto bg-[#cccccc] dark:bg-[#272727] mt-5 duration-300 rounded-lg p-5">
       <DataGrid
@@ -74,9 +83,6 @@ const DataTable = () => {
           "& .MuiSelect-iconStandard": {
             color: theme === "dark" ? "#e0e0e0" : "#6e6e6e",
           },
-          //  "& .MuiTablePagination-actions": {
-          //    color: theme === "dark" ? "#e0e0e0" : "#6e6e6e",
-          //  },
           "& .MuiTablePagination-root": {
             color: theme === "dark" ? "#e0e0e0" : "#6e6e6e",
           },
@@ -99,7 +105,6 @@ const DataTable = () => {
         }}
         pageSizeOptions={[5, 10]}
         checkboxSelection
-        //   className="border border-[#5c5c5c] dark:text-white"
       />
     </div>
   );
