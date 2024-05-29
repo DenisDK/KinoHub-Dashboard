9e7891e81e71ee8ebaa3d3e4971f00d193264fed
joi666 <shaparenkoartem@icloud.com>
2024-05-29 22:23:47 +0300

    Added info and icon to helpcards

--- src/app/Help/page.jsx
+++ src/app/Help/page.jsx
@@ -6,75 +6,57 @@ const HelpPage = () => {
     <div className="flex mt-5">
       <div className="flex justify-between gap-5 flex-wrap">
         <HelpCard
-          title={"Helpcard 1"}
+          title={"Як переглянути інформацію про свої списки?"}
           text={
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, inventore. lorem ewjfwoiejfoiwjemfweijfie wofwoi joewi fowieLorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, inventore. lorem ewjfwoiejfoiwjemfweijfie wofwoi joewi fowie jfoiwe jfowiej fowi efowe hf woiefwoi jefwoij ef"
+            "Упевніться, що ви зайшли у свій аккаунт на сайті. Після цього ви зможете переглянути інформацію про свої списки в розділі \"Мої списки\". Або ж ви можете перевірити свої списки у мобільному застосунку, увійшовши у свій аккаунт."
           }
         />
         <HelpCard
-          title={"Helpcard 2"}
+          title={"Де знайти ваш мобільний застосунок?"}
           text={
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, inventore."
+            "Наш мобільний застосунок можна знайти на головній сторінці сайту. Натисніть на кнопку \"Завантажити застосунок\", щоб перейти до завантаження та встановлення."
           }
         />
         <HelpCard
-          title={"Helpcard 222"}
+          title={"Як додати більше друзів?"}
           text={
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, inventore."
+            "Щоб додати більше друзів, придбайте преміум підписку. Преміум підписка надає можливість розширити список ваших друзів та користуватися іншими ексклюзивними функціями. Ви можете оформити преміум підписку у відповідному розділі \"Преміум\"."
           }
         />
         <HelpCard
-          title={"Helpcard 222"}
+          title={"Чи можна подарувати преміум підписку другу?"}
           text={
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, inventore."
+            "Так, ви можете подарувати преміум підписку другу. Для цього зайдіть на профіль вашого друга та натисніть на кнопку \"Подарувати\". Дотримуйтесь інструкцій на екрані, щоб завершити покупку та зробити приємний сюрприз."
           }
         />
         <HelpCard
-          title={"Helpcard 222"}
+          title={"Як залишити комментарій під кінофільмом?"}
           text={
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, inventore."
+            "Щоб залишити коментар під кінофільмом, перейдіть до самого низу сторінки з інформацією про кінофільм. Там ви знайдете секцію для коментарів, де зможете написати та опублікувати свій відгук."
           }
         />
         <HelpCard
-          title={"Helpcard 222"}
+          title={"Як змінити фото профілю?"}
           text={
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, inventore."
+            "Щоб змінити фото профілю, перейдіть до налаштувань профілю у вашому акаунті. Там ви зможете завантажити нове зображення. Якщо ви хочете встановити анімований аватар, ця функція доступна лише для користувачів з преміум підпискою. Ви можете придбати преміум підписку у відповідному розділі налаштувань."
           }
         />
         <HelpCard
-          title={"Helpcard 222"}
+          title={"Чи можна завантажити застосунок на iOS?"}
           text={
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, inventore."
+            "На жаль, на даний момент наш застосунок недоступний для завантаження на iOS. Ми активно працюємо над розробкою версії для цієї платформи. Щоб бути в курсі останніх новин та оновлень, будь ласка, стежте за інформацією на нашій сторінці \"Оновлення\". Там ви знайдете актуальні відомості про розвиток та випуск нових версій застосунку."
           }
         />
         <HelpCard
-          title={"Helpcard 222"}
+          title={"Чи залишилися ще питання?"}
           text={
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, inventore."
+            "Якщо у вас залишилися питання, слідкуйте за оновленнями у відділі \"Оновлення\". Там ви знайдете найсвіжішу інформацію та відповіді на ваші запитання."
           }
         />
         <HelpCard
-          title={"Helpcard 222"}
+          title={"Чи залишилися ще питання?"}
           text={
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, inventore."
-          }
-        />
-        <HelpCard
-          title={"Helpcard 222"}
-          text={
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, inventore."
-          }
-        />
-        <HelpCard
-          title={"Helpcard 222"}
-          text={
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, inventore."
-          }
-        />
-        <HelpCard
-          title={"Helpcard 222"}
-          text={
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, inventore."
+            "Якщо у вас залишилися питання, слідкуйте за оновленнями у відділі \"Оновлення\". Там ви знайдете найсвіжішу інформацію та відповіді на ваші запитання."
           }
         />
       </div>
--- src/components/Card/HelpCard/HelpCard.jsx
+++ src/components/Card/HelpCard/HelpCard.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
+//Icons
+import { CiCircleInfo } from "react-icons/ci";
+
 const HelpCard = ({ title, text }) => {
     return (
         <div className="bg-[#ccc] dark:bg-[#272727] p-5 rounded-lg flex max-w-[539px]">
             <div className="flex flex-col">
-                <span className="font-bold text-2xl">{title}</span>
+                <div className="flex items-center mb-2">
+                    <CiCircleInfo className="mr-2" size={50} />
+                    <span className="font-bold text-2xl">{title}</span>
+                </div>
                 <span className="text-md font-light">{text}</span>
             </div>
         </div>
